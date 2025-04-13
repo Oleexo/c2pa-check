9b5b02f4318a26e8b267ec4aeae2e08659ae422e
Oleexo <maxime.charles1@proton.me>
2025-04-13 14:32:11 +0200

    Add file upload endpoint with C2PA validation
    Introduced a new `/check` endpoint to handle file uploads and validate them using the C2PA library. Updated dependencies to include Tokio for async file handling. Added `.dockerignore` and new helper scripts for testing and validation workflows.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,67 @@
 #[macro_use] extern crate rocket;
 
+use std::io::{Cursor, Read};
+use c2pa::Reader;
+use rocket::Config;
+use rocket::http::{Status, ContentType};
+use rocket::fs::TempFile;
+use rocket::form::Form;
+use tokio::io::AsyncReadExt;
+
+
+#[derive(FromForm)]
+struct FileUpload<'r> {
+    file: TempFile<'r>,
+}
+
+pub struct CawgValidator;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/check")]
-fn check() -> &'static str {
-    "Hello, world!"
+#[post("/check", data = "<form>")]
+async fn check(mut form: Form<FileUpload<'_>>) -> (Status, (ContentType, String)) {
+
+    // Step 1: Create a buffer to hold the file's contents
+    let mut buffer = Vec::new();
+
+    // Step 2: Open the TempFile for reading
+    let file = match form.file.open().await {
+        Ok(file) => file,
+        Err(e) => return (Status::BadRequest, (ContentType::JSON, "{ \"hi\": \"world\" }".to_string())),
+    };
+    // Step 3: Read the TempFile's contents into the buffer
+    let mut reader = tokio::io::BufReader::new(file);
+    match reader.read_to_end(&mut buffer).await {
+        Ok(_) => (),
+        Err(e) => return (Status::BadRequest, (ContentType::JSON, "{ \"hi\": \"world\" }".to_string())),
+    }
+
+    let content_type : &ContentType;
+    if let Some(ct) = form.file.content_type() {
+        content_type = ct;
+    } else {
+        return (Status::BadRequest, (ContentType::JSON, "{ \"message\": \"no content-type found\" }".to_string()));
+    }
+
+    let stream = Cursor::new(buffer);
+    // Create the reader from the stream
+    let mut reader = match Reader::from_stream(content_type.to_string().as_str(), stream) {
+        Ok(reader) => reader,
+        Err(e) => return (Status::BadRequest, (ContentType::JSON, format!("{{ \"message\": \"{}\" }}", e.to_string()).to_string())),
+    };
+
+    let json_response = reader.json();
+    (Status::Ok, (ContentType::JSON, json_response))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, check])
+    let config = Config {
+        port: 8080,
+        ..Default::default()
+    };
+    rocket::custom(config).mount("/", routes![index, check])
 }
